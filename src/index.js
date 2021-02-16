b700db01bd7b56be08be9e83ac900a4da55b5b5e
Puah Chee Hau <cheehau.puah@singtel.com>
2021-02-16 19:23:18 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import { tranverseDirectory } from './library/file'
 import { searchFileContent } from './library/string'
 import { TODO_REGEXP } from './library/constants'
 
-const run = async (path) => {
+const run = async (path, regexp) => {
     const fileFound = await tranverseDirectory(`${__dirname}/${path}`);
     fileFound.forEach(filePath => {
-        if(searchFileContent(filePath, TODO_REGEXP)) {
+        if(searchFileContent(filePath, regexp)) {
             console.log(filePath);
         }
     })
 }
 
-run('./')
+run('./', TODO_REGEXP)
